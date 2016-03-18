723049030f6eb3c2677cc00c9d997bd5fafe04a8
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-03-18 22:17:02 +0300

    no message

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -1,12 +1,16 @@
 <?php
 namespace Dfe\Stripe\Controller\Index;
+use Dfe\Stripe\Settings as S;
 class Index extends \Magento\Framework\App\Action\Action {
 	/**
 	 * @override
 	 * @see \Magento\Framework\App\Action\Action::execute()
 	 * @return \Magento\Framework\Controller\Result\Redirect
 	 */
-	public function execute() {
-		return df_controller_json(['ПРЕВЕД' => 'МЕДВЕД!']);
-	}
+	public function execute() {return df_leh(function(){
+		S::s()->init();
+		/** @var array(string => mixed) $request */
+		$request = df_json_decode(@file_get_contents('php://input'));
+		return df_controller_json($request);
+	});}
 }
