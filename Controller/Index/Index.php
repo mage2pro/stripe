cd5434052d4caa9efbf41291bef7b3fb46fbcbe0
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-03-26 20:50:06 +0300

    no message

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -2,14 +2,13 @@
 namespace Dfe\Stripe\Controller\Index;
 use Dfe\Stripe\Handler;
 use Dfe\Stripe\Settings as S;
+// 2016-03-18
+// The controller is for ADVANCED integration:
+// - capturing and refunding payments through the Stripe interface (instead of the Magento interface),
+// - reverse notifications about the chargebacks and disputes.
 class Index extends \Magento\Framework\App\Action\Action {
 	/**
 	 * 2016-03-18
-	 * @todo The controller is for ADVANCED integration:
-	 * *) capturing and refunding payments through the Stripe interface
-	 * (instead of the Magento interface),
-	 * *) reverse notifications about the chargebacks and disputes.
-	 * It will be inplemented next week in the version 1.1.
 	 * @override
 	 * @see \Magento\Framework\App\Action\Action::execute()
 	 * @return \Magento\Framework\Controller\Result\Redirect
