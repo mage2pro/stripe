ee947d3d9e2cdeed1e01813c37741508b2aca322
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-04-06 09:50:45 +0300

    no message

--- registration.php
+++ registration.php
@@ -3,4 +3,5 @@ use Magento\Framework\Component\ComponentRegistrar;
 $name = implode('_', array_map(function($part) {
 	return implode(array_map('ucfirst', explode('-', $part)));
 }, array_slice(explode(DIRECTORY_SEPARATOR, __DIR__), -2, 2)));
+$name = strtr($name, ['Core_' => 'Df_', 'Mage2pro_' => 'Dfe_']);
 ComponentRegistrar::register(ComponentRegistrar::MODULE, $name, __DIR__);
