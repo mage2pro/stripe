cecfe269f89079b41faf3676840f7bf910140852
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-04-15 08:47:53 +0300

    no message

--- view/frontend/web/list.js
+++ view/frontend/web/list.js
@@ -2,6 +2,7 @@ define([
 	'uiComponent', 'Magento_Checkout/js/model/payment/renderer-list'
 ], function(Component, rendererList) {
 	'use strict';
+	/** @type {String} */
 	var code = 'dfe_stripe';
 	if (window.checkoutConfig.payment[code].isActive) {
 		rendererList.push({type: code, component: 'Dfe_Stripe/item'});
