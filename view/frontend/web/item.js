2b78ed95a968805270ccc5f7ddf4d7449ba6ae9b
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-05-18 11:13:16 +0300

    no message

--- view/frontend/web/item.js
+++ view/frontend/web/item.js
@@ -86,7 +86,7 @@ define ([
 			Stripe.setPublishableKey(this.config('publishableKey'));
 			// 2016-03-09
 			// «Mage2.PRO» → «Payment» → «Stripe» → «Prefill the Payment Form with Test Data?»
-			/** {String|Boolean} */
+			/** @type {String|Boolean} */
 			var prefill = this.config('prefill');
 			if (prefill) {
 				this.creditCardNumber(prefill);
