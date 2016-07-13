8175f4956ad03f0cf7711cf221ec8ba8409dffa4
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-07-14 00:47:25 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -6,26 +6,23 @@ class Settings extends \Df\Payment\Settings {
 	/**
 	 * 2016-03-15
 	 * «Mage2.PRO» → «Payment» → «Stripe» → «Payment Action for a New Customer»
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	public function actionForNew($s = null) {return $this->v(__FUNCTION__, $s);}
+	public function actionForNew() {return $this->v(__FUNCTION__);}
 
 	/**
 	 * 2016-03-15
 	 * «Mage2.PRO» → «Payment» → «Stripe» → «Payment Action for a Returned Customer»
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	public function actionForReturned($s = null) {return $this->v(__FUNCTION__, $s);}
+	public function actionForReturned() {return $this->v(__FUNCTION__);}
 
 	/**
 	 * 2016-03-09
 	 * «Mage2.PRO» → «Payment» → «Stripe» → «Description»
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	public function description($s = null) {return $this->v(__FUNCTION__, $s);}
+	public function description() {return $this->v(__FUNCTION__);}
 
 	/**
 	 * 2016-03-08
@@ -39,36 +36,32 @@ class Settings extends \Df\Payment\Settings {
 	/**
 	 * 2016-03-14
 	 * «Mage2.PRO» → «Payment» → «Stripe» → «Metadata»
-	 * @param null|string|int|S $s [optional]
 	 * @return string[]
 	 */
-	public function metadata($s = null) {return $this->csv(__FUNCTION__, $s);}
+	public function metadata() {return $this->csv(__FUNCTION__);}
 
 	/**
 	 * 2016-03-09
 	 * «Mage2.PRO» → «Payment» → «Stripe» → «Prefill the Payment Form with Test Data?»
 	 * @see \Dfe\Stripe\Source\Prefill::map()
-	 * @param null|string|int|S $s [optional]
 	 * @return string|false
 	 */
-	public function prefill($s = null) {return $this->bv(__FUNCTION__, $s);}
+	public function prefill() {return $this->bv(__FUNCTION__);}
 
 	/**
 	 * 2016-03-02
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	public function publishableKey($s = null) {
-		return $this->test($s) ? $this->testPublishableKey($s) : $this->livePublishableKey($s);
+	public function publishableKey() {
+		return $this->test() ? $this->testPublishableKey() : $this->livePublishableKey();
 	}
 
 	/**
 	 * 2016-03-14
 	 * «Mage2.PRO» → «Payment» → «Stripe» → «Statement for Customer»
-	 * @param null|string|int|S $s [optional]
 	 * @return string[]
 	 */
-	public function statement($s = null) {return $this->v(__FUNCTION__, $s);}
+	public function statement() {return $this->v(__FUNCTION__);}
 
 	/**
 	 * @override
@@ -93,43 +86,38 @@ class Settings extends \Df\Payment\Settings {
 	/**
 	 * 2016-03-02
 	 * «Mage2.PRO» → «Payment» → «Stripe» → «Live Publishable Key»
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function livePublishableKey($s = null) {return $this->v(__FUNCTION__, $s);}
+	private function livePublishableKey() {return $this->v(__FUNCTION__);}
 
 	/**
 	 * 2016-03-02
 	 * «Mage2.PRO» → «Payment» → «Stripe» → «Live Secret Key»
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function liveSecretKey($s = null) {return $this->p(__FUNCTION__, $s);}
+	private function liveSecretKey() {return $this->p(__FUNCTION__);}
 
 	/**
 	 * 2016-03-02
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function secretKey($s = null) {
-		return $this->test($s) ? $this->testSecretKey($s) : $this->liveSecretKey($s);
+	private function secretKey() {
+		return $this->test() ? $this->testSecretKey() : $this->liveSecretKey();
 	}
 
 	/**
 	 * 2016-03-02
 	 * «Mage2.PRO» → «Payment» → «Stripe» → «Test Publishable Key»
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function testPublishableKey($s = null) {return $this->v(__FUNCTION__, $s);}
+	private function testPublishableKey() {return $this->v(__FUNCTION__);}
 
 	/**
 	 * 2016-03-02
 	 * «Mage2.PRO» → «Payment» → «Stripe» → «Test Secret Key»
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function testSecretKey($s = null) {return $this->p(__FUNCTION__, $s);}
+	private function testSecretKey() {return $this->p(__FUNCTION__);}
 }
 
 
