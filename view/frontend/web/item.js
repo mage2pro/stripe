18285682999767d90ef35809bd5ccdcf32b502ed
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-08-06 16:24:53 +0300

    1.2.17

--- view/frontend/web/item.js
+++ view/frontend/web/item.js
@@ -1,85 +1,76 @@
 define ([
 	'Df_Payment/js/view/payment/cc-form'
-	,'jquery'
-	, 'df'
-	, 'mage/translate'
-	, 'underscore'
 	,'Dfe_Stripe/API'
-], function(Component, $, df, $t, _, Stripe) {
-	'use strict';
-	return Component.extend({
-		defaults: {clientConfig: {id: 'dfe-stripe'}},
-		/**
-		 * 2016-03-01
-		 * 2016-03-08
-		 * Раньше реализация была такой:
-		 * return _.keys(this.getCcAvailableTypes())
-		 *
-		 * https://support.stripe.com/questions/which-cards-and-payment-types-can-i-accept-with-stripe
-		 * «Which cards and payment types can I accept with Stripe?
-		 * With Stripe, you can charge almost any kind of credit or debit card:
-		 * U.S. businesses can accept
-		  		Visa, MasterCard, American Express, JCB, Discover, and Diners Club.
-		 * Australian, Canadian, European, and Japanese businesses can accept
-		 * 		Visa, MasterCard, and American Express.»
-		 *
-		 * Не стал делать реализацию на сервере, потому что там меня не устраивал
-		 * порядок следования платёжных систем (первой была «American Express»)
-		 * https://github.com/magento/magento2/blob/cf7df72/app/code/Magento/Payment/etc/payment.xml#L10-L44
-		 * А изменить этот порядок коротко не получается:
-		 * https://github.com/magento/magento2/blob/487f5f45/app/code/Magento/Payment/Model/CcGenericConfigProvider.php#L105-L124
-		 *
-		 * @returns {String[]}
-	 	 */
-		getCardTypes: function() {
-			return ['VI', 'MC', 'AE'].concat(!this.config('isUS') ? [] : ['JCB', 'DI', 'DN']);
-		},
-		/**
-		 * 2016-03-06
-   		 * @override
-   		 */
-		getData: function () {
-			return {
-				/**
-				 * 2016-05-03
-				 * Если не засунуть «token» внутрь «additional_data»,
-				 * то получим сбой:
-				 * «Property "Token" does not have corresponding setter
-				 * in class "Magento\Quote\Api\Data\PaymentInterface»
-				 */
-				additional_data: {token: this.token}
-				,method: this.item.method
-			};
-		},
-		/**
-		 * 2016-03-08
-		 * @return {String}
-		*/
-		getTitle: function() {
-			var result = this._super();
-			return result + (!this.isTest() ? '' : ' [<b>Stripe TEST MODE</b>]');
-		},
-		/**
-		 * 2016-03-02
-		 * @return {Object}
-		*/
-		initialize: function() {
-			this._super();
-			Stripe.setPublishableKey(this.config('publishableKey'));
-			// 2016-03-09
-			// «Mage2.PRO» → «Payment» → «Stripe» → «Prefill the Payment Form with Test Data?»
-			/** @type {String|Boolean} */
-			var prefill = this.config('prefill');
-			if (prefill) {
-				this.creditCardNumber(prefill);
-				this.creditCardExpMonth(7);
-				this.creditCardExpYear(2019);
-				this.creditCardVerificationNumber(111);
-			}
-			return this;
-		},
-		pay: function() {
-			var _this = this;
+], function(parent, Stripe) {'use strict'; return parent.extend({
+	/**
+	 * 2016-03-01
+	 * 2016-03-08
+	 * Раньше реализация была такой:
+	 * return _.keys(this.getCcAvailableTypes())
+	 *
+	 * https://support.stripe.com/questions/which-cards-and-payment-types-can-i-accept-with-stripe
+	 * «Which cards and payment types can I accept with Stripe?
+	 * With Stripe, you can charge almost any kind of credit or debit card:
+	 * U.S. businesses can accept
+			Visa, MasterCard, American Express, JCB, Discover, and Diners Club.
+	 * Australian, Canadian, European, and Japanese businesses can accept
+	 * 		Visa, MasterCard, and American Express.»
+	 *
+	 * Не стал делать реализацию на сервере, потому что там меня не устраивал
+	 * порядок следования платёжных систем (первой была «American Express»)
+	 * https://github.com/magento/magento2/blob/cf7df72/app/code/Magento/Payment/etc/payment.xml#L10-L44
+	 * А изменить этот порядок коротко не получается:
+	 * https://github.com/magento/magento2/blob/487f5f45/app/code/Magento/Payment/Model/CcGenericConfigProvider.php#L105-L124
+	 *
+	 * @returns {String[]}
+	 */
+	getCardTypes: function() {
+		return ['VI', 'MC', 'AE'].concat(!this.config('isUS') ? [] : ['JCB', 'DI', 'DN']);
+	},
+	/**
+	 * 2016-03-06
+	 * @override
+	 */
+	getData: function () {
+		return {
+			/**
+			 * 2016-05-03
+			 * Если не засунуть «token» внутрь «additional_data»,
+			 * то получим сбой:
+			 * «Property "Token" does not have corresponding setter
+			 * in class "Magento\Quote\Api\Data\PaymentInterface»
+			 */
+			additional_data: {token: this.token}
+			,method: this.item.method
+		};
+	},
+	/**
+	 * 2016-03-02
+	 * @return {Object}
+	*/
+	initialize: function() {
+		this._super();
+		Stripe.setPublishableKey(this.config('publishableKey'));
+		// 2016-03-09
+		// «Mage2.PRO» → «Payment» → «Stripe» → «Prefill the Payment Form with Test Data?»
+		/** @type {String|Boolean} */
+		var prefill = this.config('prefill');
+		if (prefill) {
+			this.creditCardNumber(prefill);
+			this.creditCardExpMonth(7);
+			this.creditCardExpYear(2019);
+			this.creditCardVerificationNumber(111);
+		}
+		return this;
+	},
+	/**
+	 * @override
+	 * @see https://github.com/magento/magento2/blob/2.1.0/app/code/Magento/Checkout/view/frontend/web/js/view/payment/default.js#L127-L159
+	 * @used-by https://github.com/magento/magento2/blob/2.1.0/lib/web/knockoutjs/knockout.js#L3863
+	 * @param {this} _this
+	*/
+	placeOrder: function(_this) {
+		if (this.validate()) {
 			// 2016-03-02
 			// https://stripe.com/docs/custom-form#step-2-create-a-single-use-token
 			/**
@@ -101,7 +92,7 @@ define ([
 				},
 				/**
 				 * 2016-03-02
-			 	 * @param {Number} status
+				 * @param {Number} status
 				 * @param {Object} response
 				 */
 				function(status, response) {
@@ -110,17 +101,15 @@ define ([
 						// 2016-03-02
 						// https://stripe.com/docs/custom-form#step-3-sending-the-form-to-your-server
 						_this.token = response.id;
-						_this.placeOrder();
+						_this.placeOrderInternal();
 					}
 					else {
 						// 2016-03-02
 						// https://stripe.com/docs/api#errors
-						_this.messageContainer.addErrorMessage({
-							'message': $t(response.error.message)
-						});
+						_this.showErrorMessage(response.error.message);
 					}
 				}
 			);
 		}
-	});
-});
+	}
+});});
