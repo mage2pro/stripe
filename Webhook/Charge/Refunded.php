efad42da301358e83a5de839d9abab77b2ef5620
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-01-18 15:27:38 +0300

    1.6.32

--- Webhook/Charge/Refunded.php
+++ Webhook/Charge/Refunded.php
@@ -5,7 +5,7 @@ use Dfe\Stripe\Method as M;
 final class Refunded extends \Dfe\Stripe\Webhook\Charge implements \Df\StripeClone\Webhook\IRefund {
 	/**
 	 * 2017-01-17
-	 * В валюте заказа (платежа).
+	 * В валюте заказа (платежа), в формате платёжной системы (копейках).
 	 * @override
 	 * @see \Df\StripeClone\Webhook\IRefund::amount()
 	 * @used-by \Df\StripeClone\WebhookStrategy\Charge\Refunded::handle()
