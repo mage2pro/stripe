716226250fe72f5ff50b2c9f65fae74b16b57837
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-08 21:09:36 +0300

    no message

--- Method.php
+++ Method.php
@@ -7,6 +7,7 @@ use Magento\Sales\Model\Order\Invoice;
 use Magento\Sales\Model\Order\Payment as OP;
 use Magento\Sales\Model\Order\Payment\Transaction as T;
 use Stripe\Error\Base as lException;
+/** @method Settings s() */
 final class Method extends \Df\StripeClone\Method {
 	/**
 	 * 2016-03-08
@@ -28,6 +29,26 @@ final class Method extends \Df\StripeClone\Method {
 	protected function amountFactorTable() {return [
 		1 => 'BIF,CLP,DJF,GNF,JPY,KMF,KRW,MGA,PYG,RWF,VND,VUV,XAF,XOF,XPF'
 	];}
+
+	/**
+	 * 2017-02-08
+	 * @override
+	 * Результат — в рублях, не в копейках.
+	 *
+	 * 1) «What is the minimum amount I can charge with Stripe?»
+	 * https://support.stripe.com/questions/what-is-the-minimum-amount-i-can-charge-with-stripe
+	 *
+	 * 2) «What is the maximum amount I can charge with Stripe?»
+	 * https://support.stripe.com/questions/what-is-the-maximum-amount-i-can-charge-with-stripe
+	 * «Regardless of the currency, there is a technical limitation of 8 digits
+	 * (e.g. $999,999.99, or ¥99,999,999), though we don’t impose any other limitations atop that.»
+	 * @see \Df\Payment\Method::amountLimits()
+	 * @used-by isAvailable()
+	 * @return array(string => array(int|float))
+	 */
+	protected function amountLimits() {return function($c) {return [
+		$this->minimumAmount($c), $this->amountParse(99999999)
+	];};}
 
 	/**
 	 * 2016-12-28
@@ -282,4 +303,38 @@ final class Method extends \Df\StripeClone\Method {
 			]
 		);
 	}
+
+	/**
+	 * 2017-02-08
+	 * Результат — в рублях, не в копейках.
+	 * «If your business is based in the United States and only processes payments in US dollars,
+	 * then it’s pretty straightforward.
+	 * In order to make sure each charge covers the necessary fees
+	 * and you don’t end up losing money on the transaction,
+	 * we require a minimum of $0.50 for each charge.
+	 *
+	 * If your business is based in the United States and you process payments in other currencies,
+	 * then the minimum will be the equivalent of $0.50 after the charge is converted to US dollars.»
+	 * https://support.stripe.com/questions/what-is-the-minimum-amount-i-can-charge-with-stripe#businesses-in-the-united-states
+	 *
+	 * «For businesses that are not based in the United States,
+	 * the minimum charge depends on the currency for your bank account.
+	 *
+	 * Charges that are made in the same currency as your bank account
+	 * will have the minimum shown in the list below.
+	 * Charges that are made in other currencies will have the equivalent minimum
+	 * after the charge is converted to your bank account’s currency.»
+	 *
+	 * https://support.stripe.com/questions/what-is-the-minimum-amount-i-can-charge-with-stripe#but-what-if-my-business-isnt-based-in-the-united-states
+	 *
+	 * @used-by amountLimits()
+	 * @param string $c
+	 * @return float
+	 */
+	private function minimumAmount($c) {return
+		$this->s()->isMerchantInUS() ? df_currency_convert(.5, 'USD', $c) : dfa([
+			'AUD' => .5, 'CAD' => .5, 'CHF' => .5, 'DKK' => 2.5, 'EUR' => .5, 'GBP' => .3
+			,'HKD' => 4, 'JPY' => 50, 'MXN' => 10, 'NOK' => 3, 'SEK' => 3, 'SGD' => .5, 'USD' => .5
+		], $c, .5)
+	;}
 }
