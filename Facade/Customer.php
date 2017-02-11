2486cb081bc1a0c79eb64c7709545e608b33d0be
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-11 08:04:26 +0300

    no message

--- Facade/Customer.php
+++ Facade/Customer.php
@@ -22,6 +22,7 @@ final class Customer extends \Df\StripeClone\Facade\Customer {
 	 * @used-by \Df\StripeClone\ConfigProvider::cards()
 	 * @param C $c
 	 * @return array(string => string)
+	 * [card ID => card label]
 	 */
 	public function cards($c) {return array_map(function(\Stripe\Card $card) {return [
 		'id' => $card->{'id'}, 'label' => (string)(new Card($card->__toArray()))
