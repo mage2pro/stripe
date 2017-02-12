27fe6bed57187385afead0812d050b49f4e9d024
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-12 06:51:32 +0300

    no message

--- ResponseRecord.php
+++ ResponseRecord.php
@@ -4,9 +4,10 @@ namespace Dfe\Stripe;
 final class ResponseRecord extends \Df\StripeClone\ResponseRecord {
 	/**
 	 * 2017-01-13
+	 * Returns the path to the bank card information in the payment system response.
 	 * @override
 	 * @see \Df\StripeClone\ResponseRecord::keyCard()
-	 * @used-by \Df\StripeClone\ResponseRecord::_card()
+	 * @used-by \Df\StripeClone\ResponseRecord::card()
 	 * @return string
 	 */
 	protected function keyCard() {return 'source';}
