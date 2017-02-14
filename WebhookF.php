69b651913b996a70e820acd8c58c7a6dd9a30950
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-14 05:45:52 +0300

    no message

--- WebhookF.php
+++ WebhookF.php
@@ -9,6 +9,9 @@ final class WebhookF extends \Df\StripeClone\WebhookF {
 	 * Например: https://dashboard.stripe.com/test/events/evt_19aFW1FzKb8aMux1jQV7OZ9o
 	 * Но реально этот ключ присутствует в запросе, и так было всегда с момента разработки модуля
 	 * (март 2016).
+	 * 2017-02-14
+	 * [Stripe] An example of the «charge.captured» event (being sent to a webhook)
+	 * https://mage2.pro/t/2745
 	 * @override
 	 * @see \Df\StripeClone\WebhookF::typeKey()
 	 * @used-by \Df\StripeClone\WebhookF::_class()
