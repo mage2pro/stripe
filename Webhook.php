d6881601d037954c2128214d1ab4ceb270c6b1c4
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-02-14 10:49:14 +0300

    no message

--- Webhook.php
+++ Webhook.php
@@ -8,25 +8,6 @@ namespace Dfe\Stripe;
 abstract class Webhook extends \Df\StripeClone\Webhook {
 	/**
 	 * 2017-01-04
-	 * Для Stripe-подобные платёжных систем
-	 * наш внутренний идентификатор транзакции основывается на внешнем:
-	 * <имя модуля>-<внешний идентификатор платежа>-<окончание типа события>.
-	 * 2017-01-07
-	 * Ключ должен быть именно «data/object/id».
-	 * Ключ «id» у события тоже присутствует, но его значением является не идентификатор платежа
-	 * («ch_*»), а идентификатор события («evt_*»).
-	 * 2017-02-14
-	 * [Stripe] An example of the «charge.captured» event (being sent to a webhook)
-	 * https://mage2.pro/t/2745
-	 * @override
-	 * @see \Df\Payment\Webhook::parentIdRawKey()
-	 * @used-by \Df\Payment\Webhook::parentIdRaw()
-	 * @return string
-	 */
-	final protected function parentIdRawKey() {return 'data/object/id';}
-
-	/**
-	 * 2017-01-04
 	 * 2017-01-06
 	 * Сообщение от платёжной системы — это иерархический JSON.
 	 * На верхнем уровне иерархии расположены метаданные:
