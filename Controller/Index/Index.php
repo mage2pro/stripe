60f68a0c991fabf1c7c1ce32130f42cd113b6416
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-09 17:13:40 +0300

    no message

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -1,5 +1,5 @@
 <?php
 namespace Dfe\Stripe\Controller\Index;
 // 2017-02-14
-/** @final Unable to use the PHP «final» keyword because of the M2 code generation. */
+/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
 class Index extends \Df\Payment\Action\Webhook {}
