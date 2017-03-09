4a2b93bdfb3e33c638f61790a207a15e72e451b9
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-09 23:38:30 +0300

    no message

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -2,4 +2,4 @@
 namespace Dfe\Stripe\Controller\Index;
 // 2017-02-14
 /** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
-class Index extends \Df\Payment\Action\Webhook {}
+class Index extends \Df\Payment\WebhookA {}
