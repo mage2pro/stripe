04242d2b5fb6dba911387d09d2b5c652aefcc5e6
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-09 20:30:29 +0300

    no message

--- WebhookF.php
+++ WebhookF.php
@@ -13,8 +13,8 @@ final class WebhookF extends \Df\StripeClone\WebhookF {
 	 * [Stripe] An example of the «charge.captured» event (being sent to a webhook)
 	 * https://mage2.pro/t/2745
 	 * @override
-	 * @see \Df\StripeClone\WebhookF::typeKey()
-	 * @used-by \Df\StripeClone\WebhookF::type()
+	 * @see \Df\Payment\WebhookF\Json::typeKey()
+	 * @used-by \Df\Payment\WebhookF\Json::type()
 	 * @return string
 	 */
 	protected function typeKey() {return 'type';}
