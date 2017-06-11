9f4201217b84d9fa187ffa7859c2f70126af57d9
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-11 08:43:08 +0300

    no message

--- Facade/Charge.php
+++ Facade/Charge.php
@@ -15,8 +15,7 @@ final class Charge extends \Df\StripeClone\Facade\Charge {
 	 * @used-by \Df\StripeClone\Method::charge()
 	 * @param string $id
 	 * @param int|float $a
-	 * The $a value is already converted to the payment service provider currency
-	 * and formatted according to the payment service provider requirements.
+	 * The $a value is already converted to the PSP currency and formatted according to the PSP requirements.
 	 * @return C
 	 */
 	function capturePreauthorized($id, $a) {return C::retrieve($id)->capture(['amount' => $a]);}
