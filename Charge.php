d8144f7c9ff3caf8615ee2259237aa4fe19bb758
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-06-11 09:57:45 +0300

    no message

--- Charge.php
+++ Charge.php
@@ -143,8 +143,8 @@ final class Charge extends \Df\StripeClone\Charge {
 	 * У Stripe название этого параметра для обоих запросов совпадает.
 	 * @override
 	 * @see \Df\StripeClone\Charge::k_CardId()
-	 * @used-by \Df\StripeClone\Charge::request()
 	 * @used-by \Df\StripeClone\Charge::newCard()
+	 * @used-by \Df\StripeClone\Charge::request()
 	 * @return string
 	 */
 	protected function k_CardId() {return 'source';}
