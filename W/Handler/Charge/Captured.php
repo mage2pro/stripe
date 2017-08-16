5a4c744ad778b97b416cc3054b0cd859dea6335c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-08-16 09:58:13 +0300

    no message

--- W/Handler/Charge/Captured.php
+++ W/Handler/Charge/Captured.php
@@ -1,7 +1,8 @@
 <?php
 namespace Dfe\Stripe\W\Handler\Charge;
-use Df\Payment\W\Strategy\CapturePreauthorized as Strategy;
 // 2017-01-04
+// 2017-08-16 We get this event when the merchant has just captured a preauthorized payment
+// from his Stripe dashboard.
 final class Captured extends \Df\Payment\W\Handler {
 	/**
 	 * 2017-03-13
@@ -10,5 +11,5 @@ final class Captured extends \Df\Payment\W\Handler {
 	 * @used-by \Df\Payment\W\Handler::handle()
 	 * @return string
 	 */
-	protected function strategyC() {return Strategy::class;}	
+	protected function strategyC() {return \Df\Payment\W\Strategy\CapturePreauthorized::class;}
 }
