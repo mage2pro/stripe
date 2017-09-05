a459f6350b88b885742b1b332cdb053de0e4f49d
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-09-06 02:39:22 +0300

    no message

--- view/frontend/web/main.js
+++ view/frontend/web/main.js
@@ -1,6 +1,9 @@
+// 2016-03-01
 define([
 	'Df_StripeClone/main', 'Magento_Checkout/js/model/quote', 'https://js.stripe.com/v2/'
-], function(parent, quote) {'use strict'; return parent.extend({
+], function(parent, quote) {'use strict';
+/** 2017-09-06 @uses Class::extend() https://github.com/magento/magento2/blob/2.2.0-rc2.3/app/code/Magento/Ui/view/base/web/js/lib/core/class.js#L106-L140 */	
+return parent.extend({
 	/**
 	 * 2016-03-01
 	 * 2016-03-08
