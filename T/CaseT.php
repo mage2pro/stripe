34b86aa0f3113e77e2272de0c51aacff812f97ca
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-10-19 06:53:02 +0300

    no message

--- T/CaseT.php
+++ T/CaseT.php
@@ -5,4 +5,11 @@ namespace Dfe\Stripe\T;
  * @see \Dfe\Stripe\T\CaseT\CountrySpec
  * @method \Dfe\Stripe\Method m()
  */
-abstract class CaseT extends \Df\Payment\TestCase {}
+abstract class CaseT extends \Df\Payment\TestCase {
+	/**
+	 * 2017-10-19
+	 * @override
+	 * @see \Df\Core\TestCase::setUp()
+	 */
+	protected function setUp() {parent::setUp(); $this->m()->s()->init();}
+}
--- T/CaseT/CountrySpec.php
+++ T/CaseT/CountrySpec.php
@@ -1,7 +1,19 @@
 <?php
 namespace Dfe\Stripe\T\CaseT;
+use Stripe\CountrySpec as lCountrySpec;
 // 2017-10-19
+// https://github.com/stripe/stripe-php/blob/v5.3.0/tests/CountrySpecTest.php
+// «Stripe API Reference» → «Country Specs»: https://stripe.com/docs/api#country_specs
 final class CountrySpec extends \Dfe\Stripe\T\CaseT {
 	/** @test 2017-10-19 */
 	function t00() {}
+
+	/**
+	 * @test 2017-10-19
+	 * https://stripe.com/docs/api#country_spec_object-supported_payment_methods
+	 */
+	function t01() {
+		echo df_json_encode(lCountrySpec::retrieve('US')['supported_payment_methods']);
+		echo df_json_encode(lCountrySpec::retrieve('NL')['supported_payment_methods']);
+	}
 }
