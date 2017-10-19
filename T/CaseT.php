7cf10d179e1bfe771845a4f2d772a227b124b7ec
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-10-19 06:26:59 +0300

    no message

---
+++ T/CaseT.php
@@ -0,0 +1,8 @@
+<?php
+namespace Dfe\Stripe\T;
+/**
+ * 2017-10-19
+ * @see \Dfe\Stripe\T\CaseT\CountrySpec
+ * @method \Dfe\Stripe\Method m()
+ */
+abstract class CaseT extends \Df\Payment\TestCase {}
---
+++ T/CaseT/CountrySpec.php
@@ -0,0 +1,7 @@
+<?php
+namespace Dfe\Stripe\T\CaseT;
+// 2017-10-19
+final class CountrySpec extends \Dfe\Stripe\T\CaseT {
+	/** @test 2017-10-19 */
+	function t00() {}
+}
