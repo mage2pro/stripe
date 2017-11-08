4273e0fe9f6b9472b9a8b806914e65b1b1b1ede3
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-11-08 20:30:28 +0300

    no message

--- W/Event/Source.php
+++ W/Event/Source.php
@@ -4,6 +4,30 @@ namespace Dfe\Stripe\W\Event;
 final class Source extends \Dfe\Stripe\W\Event {
 	/**
 	 * 2017-11-08
+	 * Note 1.
+	 * Stripe really sends 2 `source.chargeable` events for each payment
+	 * in the 3D Secure verification scenario:
+	 * 1) The first `source.chargeable` event is for an initial reusable source:
+	 * "An initial reusable source for a card which requires a 3D Secure verification" https://mage2.pro/t/4893
+	 * "A `source.chargeable` event for an initial reusable source for a bank card" https://mage2.pro/t/4889
+	 * It is sent before the 3D Secure verification, so I just ignore it.
+	 * 2) The first `source.chargeable` event is for a derived single-use 3D Secure source:
+	 * "A derived single-use 3D Secure source" https://mage2.pro/t/4894
+	 * "A `source.chargeable` event for a derived single-use 3D Secure source" https://mage2.pro/t/4895
+	 * It is sent after a successful 3D Secure verification, and I handle it (make a charge).
+	 * Note 1.
+	 * $this->ro('type') returns:
+	 * 		«card» for an initial reusable source
+	 * 		«three_d_secure» for a derived single-use 3D Secure source
+	 * @override
+	 * @see \Df\Payment\W\Event::isIgnored()
+	 * @used-by \Df\Payment\W\Strategy\ConfirmPending::_handle()
+	 * @return bool
+	 */
+	function isIgnored() {return 'card' === $this->ro('type');}
+
+	/**
+	 * 2017-11-08
 	 * @override
 	 * @see \Df\StripeClone\W\Event::ttCurrent()
 	 * @used-by \Df\StripeClone\W\Nav::id()
