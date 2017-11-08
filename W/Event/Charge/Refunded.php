bb3364a5833438c5cbf05316e86b8acc81ebb353
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-11-08 10:00:14 +0300

    no message

--- W/Event/Charge/Refunded.php
+++ W/Event/Charge/Refunded.php
@@ -6,7 +6,7 @@ final class Refunded extends \Dfe\Stripe\W\Event {
 	 * 2017-01-06
 	 * @override
 	 * @see \Df\StripeClone\W\Event::ttCurrent()
-	 * @used-by \Df\StripeClone\W\Event::id()
+	 * @used-by \Df\StripeClone\W\Nav::id()
 	 * @used-by \Df\Payment\W\Strategy\ConfirmPending::_handle()
 	 * @return string
 	 */
