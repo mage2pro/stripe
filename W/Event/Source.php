bdc72e7187715cbc050c3efafb41d0d0676c72ed
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-11-10 05:23:12 +0300

    no message

--- W/Event/Source.php
+++ W/Event/Source.php
@@ -5,13 +5,12 @@ final class Source extends \Dfe\Stripe\W\Event {
 	/**
 	 * 2017-11-08
 	 * Note 1.
-	 * Stripe really sends 2 `source.chargeable` events for each payment
-	 * in the 3D Secure verification scenario:
+	 * Stripe really sends 2 `source.chargeable` events for each payment in the 3D Secure verification scenario:
 	 * 1) The first `source.chargeable` event is for an initial reusable source:
 	 * "An initial reusable source for a card which requires a 3D Secure verification" https://mage2.pro/t/4893
 	 * "A `source.chargeable` event for an initial reusable source for a bank card" https://mage2.pro/t/4889
 	 * It is sent before the 3D Secure verification, so I just ignore it.
-	 * 2) The first `source.chargeable` event is for a derived single-use 3D Secure source:
+	 * 2) The second `source.chargeable` event is for a derived single-use 3D Secure source:
 	 * "A derived single-use 3D Secure source" https://mage2.pro/t/4894
 	 * "A `source.chargeable` event for a derived single-use 3D Secure source" https://mage2.pro/t/4895
 	 * It is sent after a successful 3D Secure verification, and I handle it (make a charge).
