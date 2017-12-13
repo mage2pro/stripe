563a420c645a6901b15b408c7e27ff2bab0c13ff
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-12-13 03:39:54 +0300

    no message

--- registration.php
+++ registration.php
@@ -1,11 +1,11 @@
 <?php
 use Magento\Framework\Component\ComponentRegistrar as R;
 R::register(R::MODULE, 'Dfe_Stripe', __DIR__);
-// 2017-04-25
-// К сожалению, прямолинейным образом вынести этот код
-// в повторноиспользуемую функцию df_lib() не получается:
-// в эту точку программы мы попадаем раньше инициализации повторноиспользуемой функции.
-// @todo Надо подумать, как это сделать...
+// 2017-04-25, 2017-12-13
+// Unfortunately, I have not found a way to make this code reusable among my modules.
+// I tried to move this code to a `/lib` function like df_lib(), but it raises a «chicken and egg» problem,
+// because Magento runs the `registration.php` scripts before any `/lib` functions are initalized,
+// whereas the `/lib` functions are initalized from the `registration.php` scripts.
 $base = dirname(__FILE__); /** @var string $base */
 if (is_dir($libDir = "{$base}/lib")) { /** @var string $libDir */
 	// 2015-02-06
