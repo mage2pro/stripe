dab800758f5c1a43c2dcf907854b81445762b478
Dmitry Fedyuk <dfediuk@gmail.com>
2020-01-31 14:26:29 +0600

    2.7.5

--- Test/CaseT.php
+++ Test/CaseT.php
@@ -1,8 +1,8 @@
 <?php
-namespace Dfe\Stripe\T;
+namespace Dfe\Stripe\Test;
 /**
  * 2017-10-19
- * @see \Dfe\Stripe\T\CaseT\CountrySpec
+ * @see \Dfe\Stripe\Test\CaseT\CountrySpec
  * @method \Dfe\Stripe\Method m()
  */
 abstract class CaseT extends \Df\Payment\TestCase {
--- Test/CaseT/CountrySpec.php
+++ Test/CaseT/CountrySpec.php
@@ -1,10 +1,10 @@
 <?php
-namespace Dfe\Stripe\T\CaseT;
+namespace Dfe\Stripe\Test\CaseT;
 use Stripe\CountrySpec as lCountrySpec;
 // 2017-10-19
 // https://github.com/stripe/stripe-php/blob/v5.3.0/tests/CountrySpecTest.php
 // «Stripe API Reference» → «Country Specs»: https://stripe.com/docs/api#country_specs
-final class CountrySpec extends \Dfe\Stripe\T\CaseT {
+final class CountrySpec extends \Dfe\Stripe\Test\CaseT {
 	/** @test 2017-10-19 */
 	function t00() {}
 
